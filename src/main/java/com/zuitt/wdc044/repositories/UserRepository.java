fed5a2f3c7a2a69a3b338970279a78d5680be5c8
cvanamiel12 <cvanamiel@gmail.com>
2022-07-12 18:30:36 +0800

    ActivityTwo

--- src/main/java/com/zuitt/wdc044/repositories/UserRepository.java
+++ src/main/java/com/zuitt/wdc044/repositories/UserRepository.java
@@ -1,6 +1,5 @@
 package com.zuitt.wdc044.repositories;
 
-import com.zuitt.wdc044.models.Post;
 import com.zuitt.wdc044.models.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
